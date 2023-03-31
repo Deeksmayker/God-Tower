d9da99c230c1ce257ccda4d6f386d39ffd0674e5
Elllias <ellias_vo312@mail.ru>
2023-03-31 10:17:13 +0500

    Поправил логику гранаты на бомбочку; модифицировал gitignore.
    Теперь бомбочка взрывается только при соприкосновении с поверхностью.

---
+++ Assets/Source/Logic/Utils/Timer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public static class Timer
+{
+    public static IEnumerator TakeActionAfterTime(float seconds, Action action)
+    {
+        yield return new WaitForSeconds(seconds);
+        action.Invoke();
+    }
+}
