38b677ce2f62a2a22e1839dcb7ac4484d820f172
Elllias <ellias_vo312@mail.ru>
2023-04-09 23:50:16 +0500

    Дореализовал способность кидания бомбочки.

--- Assets/Source/Logic/Fight/ActiveAbilities/GrenadeAbility.cs
+++ Assets/Source/Logic/Fight/ActiveAbilities/GrenadeAbility.cs
@@ -1,21 +1,56 @@
 ﻿using System;
+using Cinemachine.Utility;
 using NTC.Global.Pool;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class GrenadeAbility : DefaultActiveAbility
 {
-    [Header("Grenade")]
+    [Header("Grenade Settings")]
     [SerializeField] private Grenade GrenadePrefab;
 
-    [SerializeField] private float throwPower = 1;
-        
+    [SerializeField] private float throwPower = 10;
+    [SerializeField] private float maxSpread = 30;
+    [SerializeField] private int dumpedGrenadeCount = 3;
+
+    protected override void OnEnabled()
+    {
+        OnDump += OnDumpGrenadeAbility;
+    }
+    
+    protected override void OnDisabled()
+    {
+        OnDump -= OnDumpGrenadeAbility;
+    }
+
     public override void PerformAbility()
     {
         base.PerformAbility();
+        ThrowGrenades(1);
+    }
 
-        var spawnedGrenade = Instantiate(GrenadePrefab, GetStartPoint(), Quaternion.identity);
-        spawnedGrenade.transform.position = GetStartPoint();
-        spawnedGrenade.GetComponent<Rigidbody>().AddForce(GetPerformDirection() * throwPower, ForceMode.Impulse);
+    private void OnDumpGrenadeAbility()
+    {
+        ThrowGrenades(dumpedGrenadeCount);
+    }
+    
+    /// <summary>
+    /// Спавнит и придает ускорение гранатам, в количестве <paramref name="countSpawnedGrenade"/>.
+    /// Если гранат больше одной, то придает каждой рандомный разброс.
+    /// </summary>
+    /// <param name="countSpawnedGrenade"> Количество выпускаемых гранат. </param>
+    private void ThrowGrenades(int countSpawnedGrenade)
+    {
+        for (var i = 0; i < countSpawnedGrenade; i++)
+        {
+            var spawnedGrenade = Instantiate(GrenadePrefab, GetStartPoint(), directionTarget.rotation);
+            
+            spawnedGrenade.transform.Rotate(new Vector3(0,
+                countSpawnedGrenade == 1 ? 0 : Random.Range(-maxSpread, maxSpread), 0));
+            spawnedGrenade.GetComponent<Rigidbody>().velocity = spawnedGrenade.transform.forward * throwPower;
+        }
     }
 }
