566c2591df3c70572f6cafff8f4414d85e64ae64
Deeksmayker <lokkins334@gmail.com>
2023-04-20 01:19:31 +0500

    Доделал вращение камеры отчасти

--- Assets/Source/Logic/Camera/CameraRotating.cs
+++ Assets/Source/Logic/Camera/CameraRotating.cs
@@ -1,128 +1,31 @@
 using DG.Tweening;
+using NTC.Global.Cache;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Zenject;
 
-public class CameraRotating : MonoBehaviour
+public class CameraRotating : MonoCache
 {
-    /*public float _cameraRotationSpeed = 0.1f;
-    [SerializeField] private Transform cameraTransform;
+    [Inject] private PlayerInput _playerInput;
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            tiltCamera(true); // Replace parameter with bool
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            tiltCamera(false); // Replace parameter with bool
-        }
-    }
-
-    void tiltCamera(bool rotateRight)
-    {
-        float angle = cameraTransform.eulerAngles.y;
-        
-        if (rotateRight)
-            angle += _cameraRotationSpeed;
-        else
-            angle -= _cameraRotationSpeed;
-
-        angle = Mathf.Clamp(angle, 20, 70);
-
-        cameraTransform.eulerAngles = new Vector3(cameraTransform.eulerAngles.x, cameraTransform.eulerAngles.y, angle);
-    }*/
-
-    public static CameraRotating Instance;
-
-    [Inject] private PlayerUnit _player;
-    private IMover _mover;
-
-    [SerializeField] private Transform cameraTransform;
     [SerializeField] private int angleOfRotationForZ = 20;
-    [SerializeField] private int angleOfRotationForX = 40;
-    [SerializeField] private Ease easeForRotation;
-    [SerializeField] private Ease easeForReturnToStraightPosition;
-    [SerializeField] private float timeForRotation = 1;
-    [SerializeField] private float timeForReturnToStraightPosition = 1;
+    //[SerializeField] private int angleOfRotationForX = 40;
 
-    private Sequence _inertingSequence;
+    [SerializeField] private float rotationSpeed = 5;
 
-    private void Awake()
+    protected override void Run()
     {
-        Instance = this;
+        RotateHead(_playerInput.actions["Move"].ReadValue<Vector2>());
     }
 
-    private void Start()
+    private void RotateHead(Vector2 input)
     {
-        _mover = _player.Get<IMover>();
-        _inertingSequence = DOTween.Sequence();
-    }
+        var desiredZAngle = -input.x * angleOfRotationForZ;
+        
+        var desiredAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, desiredZAngle);
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            RotateToLeft();
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            RotateToRight();
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            RotateToBack();
-        }
+        var needRotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(desiredAngles), Time.deltaTime * rotationSpeed);
 
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
-        {
-            ReturnToStraightPosition();
-        }
-    }
-
-    public void RotateToRight()// z-=
-    {
-
-        //_inertingSequence.Kill();
-
-        Vector3 necessaryRotation = new Vector3(cameraTransform.rotation.x,
-            cameraTransform.rotation.y, cameraTransform.rotation.z - angleOfRotationForZ);
-
-        /*_inertingSequence.Append(cameraTransform.DORotate(necessaryRotation, timeForRotation).SetEase(easeForRotation));*/
-
-        cameraTransform.DORotate(necessaryRotation, timeForRotation).SetEase(easeForRotation);
-    }
-
-    public void RotateToLeft() // z+=
-    {
-        //_inertingSequence.Kill();
-
-        Vector3 necessaryRotation = new Vector3(cameraTransform.rotation.x,
-            cameraTransform.rotation.y, cameraTransform.rotation.z + angleOfRotationForZ);
-
-        //_inertingSequence.Append(cameraTransform.DORotate(necessaryRotation, timeForRotation).SetEase(easeForRotation));
-        cameraTransform.DORotate(necessaryRotation, timeForRotation).SetEase(easeForRotation);
-
-    }
-
-    public void RotateToBack() // x-=
-    {
-        //_inertingSequence.Kill();
-
-        Vector3 necessaryRotation = new Vector3(cameraTransform.rotation.x - angleOfRotationForX,
-            cameraTransform.rotation.y, cameraTransform.rotation.z);
-
-        /*_inertingSequence.Append(cameraTransform.DORotate(necessaryRotation, timeForReturnToStraightPosition).SetEase(easeForRotation));*/
-
-        cameraTransform.DORotate(necessaryRotation, timeForReturnToStraightPosition).SetEase(easeForRotation);
-    }
-
-    public void ReturnToStraightPosition()
-    {
-        //_inertingSequence.Kill();
-
-        Vector3 necessaryRotation = Vector3.zero;
-        //_inertingSequence.Append(cameraTransform.DORotate(necessaryRotation, timeForReturnToStraightPosition).SetEase(easeForReturnToStraightPosition));
-        cameraTransform.DORotate(necessaryRotation, timeForReturnToStraightPosition).SetEase(easeForReturnToStraightPosition);
+        transform.rotation = needRotation;
     }
 }
