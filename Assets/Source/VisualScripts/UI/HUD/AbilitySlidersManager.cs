a0a051e2c724d246f66723cb9e011842f9ef4e77
Elllias <ellias_vo312@mail.ru>
2023-05-09 23:29:31 +0500

    Изменена логика работы виньетки при получении урона и изменении кол-ва здоровья; добавлены слайдеры времени действия способностей.

---
+++ Assets/Source/VisualScripts/UI/HUD/AbilitySlidersManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using NTC.Global.Cache;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilitySlidersManager : MonoCache
+{
+    [SerializeField] private AbilitiesHandler abilitiesHandler;
+    [SerializeField] private Slider rightAbilitySlider;
+    [SerializeField] private Slider leftAbilitySlider;
+
+    private Coroutine _checkingRightAbilityLifetime;
+    private Coroutine _checkingLeftAbilityLifetime;
+    
+    private void Start()
+    {
+        rightAbilitySlider.gameObject.SetActive(false);
+        leftAbilitySlider.gameObject.SetActive(false);
+        
+        abilitiesHandler.OnNewRightAbility += () =>
+        {
+            rightAbilitySlider.value =
+                    rightAbilitySlider.maxValue = abilitiesHandler.GetRightAbility().GetMaxLifetime();
+            
+            if (_checkingRightAbilityLifetime != null) 
+                StopCoroutine(_checkingRightAbilityLifetime);
+            _checkingRightAbilityLifetime = StartCoroutine(
+                StartCheckingAbilityLifetime(rightAbilitySlider, abilitiesHandler.GetRightAbility()));
+            
+            abilitiesHandler.GetRightAbility().OnDump += () =>
+            {
+                _checkingRightAbilityLifetime = null;
+                rightAbilitySlider.gameObject.SetActive(false);
+            };
+        };
+        abilitiesHandler.OnNewLeftAbility += () =>
+        {
+            leftAbilitySlider.value =
+                    leftAbilitySlider.maxValue = abilitiesHandler.GetLeftAbility().GetMaxLifetime();
+            
+            if (_checkingLeftAbilityLifetime != null) 
+                StopCoroutine(_checkingLeftAbilityLifetime);
+            _checkingLeftAbilityLifetime = StartCoroutine(
+                StartCheckingAbilityLifetime(leftAbilitySlider, abilitiesHandler.GetLeftAbility()));
+            
+            abilitiesHandler.GetLeftAbility().OnDump += () =>
+            {
+                _checkingLeftAbilityLifetime = null;
+                leftAbilitySlider.gameObject.SetActive(false);
+            };
+        };
+    }
+
+    private IEnumerator StartCheckingAbilityLifetime(Slider slider, IActiveAbility activeAbility)
+    {
+        slider.gameObject.SetActive(true);
+        
+        while (slider.value > 0)
+        {
+            slider.value = activeAbility.GetRemainingLifetime();
+            yield return new WaitForFixedUpdate();
+        }
+
+        slider.gameObject.SetActive(false);
+    }
+}
